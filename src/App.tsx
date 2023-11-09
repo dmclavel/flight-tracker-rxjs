e9ab026272f78e3e00669464251b61cebbf9c7a4
Daimler Clavel <daimlerclavel@Daimlers-MacBook-Air.local>
2023-11-09 11:43:57 +0800

    Initial commit to master

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react';
+import { Grommet } from 'grommet';
+
+import theme from './theme';
+
+function App() {
+
+  return (
+    <Grommet theme={theme}>
+      
+    </Grommet>
+  );
+}
+
+export default App
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+export default Object.freeze({
+  global: {
+    font: {
+      family: "Roboto",
+    },
+  },
+});
